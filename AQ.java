db9b5b17b855542bfcf1737357de2c290493a77a
berricoding <nwesaungpan@gmail.com>
2026-01-30 16:07:28 +0000

    DSA lab #queue1

---
+++ AQ.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class AQ{
+
+    public static void main(String[]args){
+        ArrayQueue AQ = new ArrayQueue();
+    Scanner scanner = new Scanner(System.in);
+    while(true){
+    System.out.println("1.Enqueue");
+    System.out.println("2.Dequeue");
+    System.out.println("3.Display");
+    System.out.println("4.Exit");
+int num = scanner.nextInt();
+switch(num){
+    case 1:
+        System.out.println("Add a number");
+        int n = scanner.nextInt();
+        AQ.enqueue(n);
+        break;
+    case 2:
+        AQ.dequeue();
+        break;
+    case 3:
+       AQ.display();
+    case 4:
+        scanner.close();
+        return;
+        default:
+            System.out.println("Invalid Option! Please choose 1-4");
+}
+    }
+
+
+}
+}
---
+++ ArrayQueue.java
@@ -0,0 +1,73 @@
+//Array based ordinary Queue
+
+class ArrayQueue {
+public static final int CAPACITY = 1000; // default array capacity
+private int[] data; // integer array used for storage
+private int f = 0; // front pointer
+private int r = -1; // rear pointer
+private int sz = 0; // No. of elements in the Queue
+private int N; // Queue max. size
+
+public ArrayQueue() {
+this(CAPACITY);
+} // constructs the queue with default capacity
+public ArrayQueue(int capacity) { // constructs stack with given capacity
+data = new int[capacity];
+N = capacity;
+}
+public int size() {
+return sz;
+}
+public boolean isEmpty( ) {
+return (f > r);
+}
+public void enqueue(int e) {
+    if(r==N-1){
+        System.out.println("Sorry,this is overflow");
+   
+    }else{
+          data[++r]= e;
+          sz++;
+    }
+   
+    
+}
+public int peek() {
+    if(isEmpty()){
+        System.out.println("Sorr,this is underflow");
+        return -999;
+    }else
+    return data[f];
+}
+public int dequeue() {
+   if(isEmpty()){
+        System.out.println("Sorry,this is underflow");
+        return -1;
+   }
+   int out = data[f];
+   if(f==r){
+    f=0;
+    r=-1;
+   }else{
+    f++;
+     
+   }
+   return out;
+  
+}
+public void display(){
+    if(isEmpty()){
+        System.out.println("Sorry,this is underflow");
+    }else {
+    System.out.println("AQ list: ");
+     for(int i=f; i <=r; i++ ){
+     System.out.print(data[i]+" ");
+     System.out.println();
+     
+     }
+     
+     
+    }
+
+}
+}
